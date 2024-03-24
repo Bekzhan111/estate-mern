4e65e34017ad5028b81933117e98502a0d5485db
Bekzhan <abdullayev.beka.2003@gmail.com>
2024-03-24 15:43:15 +0500

    create sign in api route

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,7 @@
-export default errorHandler = (statusCode, message) => {
-  const error = new Error();
+const errorHandler = (statusCode, message) => {
+  const error = new Error(message);
   error.statusCode = statusCode;
-  error.message = message;
   return error;
 };
+
+export default errorHandler;
